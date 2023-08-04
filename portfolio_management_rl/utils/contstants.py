03c519d7c3057be7448d9d76115d876ab78fec35
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2023-08-05 00:47:41 +0200

    Created utility files for logging and constants

---
+++ portfolio_management_rl/utils/contstants.py
@@ -0,0 +1,8 @@
+"""
+This file contains all the constants used in the project.
+"""
+
+from pathlib import Path
+
+# Path to the root directory of the project
+PROJECT_DIR = Path(__file__).resolve().parents[2]
